59ad8fea3c0a6a8af31647ac19b7f1c7677b32e4
Guilty03 <innocent04102003@gmail.com>
2024-12-02 18:53:23 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;  // Declare globally
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';  // Set global bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';  // Overwrite global bestCustomer
+}
+
+const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Charlie';  // This will fail, as leastFavoriteCustomer is a constant
+}
